4891df1d101876b886871d3c47a981760820e178
Jake <ram19018@byui.edu>
2023-02-24 17:50:04 -0500

    All but 3 tests pass

--- set.h
+++ set.h
@@ -13,7 +13,7 @@
 *        set                 : A class that represents a Set
 *        set::iterator       : An iterator through Set
 * Author
-*    <Your name here>
+*    Jake Rammell, Austin Elderidge
 ************************************************************************/
 
 #pragma once
@@ -29,265 +29,281 @@ class TestSet;        // forward declaration for unit tests
 namespace custom
 {
 
-/************************************************
- * SET
- * A class that represents a Set
- ***********************************************/
-template <typename T>
-class set
-{
-   friend class ::TestSet; // give unit tests access to the privates
-public:
-   
-   // 
-   // Construct
-   //
-   set() :
-       bst{ BST<T>() } //just instantiate a new BST blank
-   {
-   }
-   set(const set &  rhs) : 
-   bst{ BST<T>(rhs.bst)}
-   { 
-   }
-   set(set && rhs) :
-   bst{ BST<T>(std::move(rhs.bst)) } //move the BST using the move constructor
-   { 
-   }
-   set(const std::initializer_list <T> & il) :
-       bst{BST<T>(il)}
-   {
-   }
-   template <class Iterator>
-   set(Iterator first, Iterator last) 
-   {
-       insert(first, last); //insert all elements between the two iterators
-   }
-   ~set() { clear(); }
+    /************************************************
+     * SET
+     * A class that represents a Set
+     ***********************************************/
+    template <typename T>
+    class set
+    {
+        friend class ::TestSet; // give unit tests access to the privates
+    public:
 
-   //
-   // Assign
-   //
+        // 
+        // Construct
+        //
+        set() :
+            bst{ BST<T>() } //just instantiate a new BST blank
+        {
+        }
+        set(const set& rhs) :
+            bst{ BST<T>(rhs.bst) }
+        {
+        }
+        set(set&& rhs) :
+            bst{ BST<T>(std::move(rhs.bst)) } //move the BST using the move constructor
+        {
+        }
+        set(const std::initializer_list <T>& il) :
+            bst{ BST<T>(il) }
+        {
+        }
+        template <class Iterator>
+        set(Iterator first, Iterator last)
+        {
+            insert(first, last); //insert all elements between the two iterators
+        }
+        ~set() { clear(); }
 
-   set & operator = (const set & rhs)
-   {
-       clear();
-       bst = rhs.bst;
-      return *this;
-   }
-   set & operator = (set && rhs)
-   {
-       clear();
-       bst.swap(rhs.bst);
-      return *this;
-   }
-   set & operator = (const std::initializer_list <T> & il)
-   {
-       clear();
-       for (auto element : il) {
-           bst.insert(element);
-       }
-      return *this;
-   }
-   void swap(set& rhs) noexcept
-   {
-       bst.swap(rhs.bst);
-   }
+        //
+        // Assign
+        //
 
-   //
-   // Iterator
-   //
+        set& operator = (const set& rhs)
+        {
+            clear();
+            bst = rhs.bst;
+            return *this;
+        }
+        set& operator = (set&& rhs)
+        {
+            clear();
+            bst.swap(rhs.bst);
+            return *this;
+        }
+        set& operator = (const std::initializer_list <T>& il)
+        {
+            clear();
+            for (auto element : il) {
+                bst.insert(element);
+            }
+            return *this;
+        }
+        void swap(set& rhs) noexcept
+        {
+            bst.swap(rhs.bst);
+        }
 
-   class iterator;
-   iterator begin() const noexcept 
-   { 
-      return iterator(bst.begin()); 
-   }
-   iterator end() const noexcept 
-   { 
-      return iterator(bst.end()); 
-   }
+        //
+        // Iterator
+        //
 
-   //
-   // Access
-   //
-   iterator find(const T& t) 
-   { 
-      return iterator(bst.find(t)); 
-   }
+        class iterator;
+        iterator begin() const noexcept
+        {
+            return iterator(bst.begin());
+        }
+        iterator end() const noexcept
+        {
+            return iterator(bst.end());
+        }
 
-   //
-   // Status
-   //
-   bool   empty() const noexcept 
-   { 
-      return true;    
-   }
-   size_t size() const noexcept 
-   { 
-      return 99;     
-   }
+        //
+        // Access
+        //
+        iterator find(const T& t)
+        {
+            return iterator(bst.find(t));
+        }
 
-   //
-   // Insert
-   //
-   std::pair<iterator, bool> insert(const T& t)
-   {
-      std::pair<iterator, bool> p(iterator(), true);
-      return p;
-   }
-   std::pair<iterator, bool> insert(T&& t)
-   {
-      std::pair<iterator, bool> p(iterator(), true);
-      return p;
-   }
-   void insert(const std::initializer_list <T>& il)
-   {
-   }
-   template <class Iterator>
-   void insert(Iterator first, Iterator last)
-   {
-   }
+        //
+        // Status
+        //
+        bool   empty() const noexcept
+        {
+            return bst.empty();
+        }
+        size_t size() const noexcept
+        {
+            return bst.size();
+        }
 
-
-   //
-   // Remove
-   //
-   void clear() noexcept 
-   { 
-   }
-   iterator erase(iterator &it)
-   { 
-      return iterator(); 
-   }
-   size_t erase(const T & t) 
-   {
-      return 99;
-   }
-   iterator erase(iterator &itBegin, iterator &itEnd)
-   {
-      return iterator();
-   }
-
-private:
-   
-   custom::BST <T> bst;
-};
+        //
+        // Insert
+        //
+        std::pair<iterator, bool> insert(const T& t)
+        {
+            std::pair<iterator, bool> bstPair = bst.insert(t, true);
+            return bstPair;
+        }
+        std::pair<iterator, bool> insert(T&& t)
+        {
+            std::pair<iterator, bool> bstPair = bst.insert(t, true);
+            return bstPair;
+        }
+        void insert(const std::initializer_list <T>& il)
+        {
+            for (auto element : il)
+            {
+                insert(element);
+            }
+        }
+        template <class Iterator>
+        void insert(Iterator first, Iterator last)
+        {
+            while (first != last)
+            {
+                insert((*first));
+                first++;
+            }
+        }
 
 
-/**************************************************
- * SET ITERATOR
- * An iterator through Set
- *************************************************/
-template <typename T>
-class set <T> :: iterator
-{
-   friend class ::TestSet; // give unit tests access to the privates
-   friend class custom::set<T>;
+        //
+        // Remove
+        //
+        void clear() noexcept
+        {
+            bst.clear();
+        }
+        iterator erase(iterator& it)
+        {
+            return iterator(bst.erase(it.it));
+        }
+        size_t erase(const T& t)
+        {
+            iterator it = find(t);
+            if (it == end())
+                return 0;
 
-public:
-   // constructors, destructors, and assignment operator
-   iterator() : it{BST<T>::iterator()}
-   { 
-   }
-   iterator(const typename custom::BST<T>::iterator& itRHS) : it{BST<T>::iterator(itRHS)} //use the paramater BST iterator to create our own
-   {  
-   }
-   iterator(const iterator & rhs) : it{BST<T>::iterator(rhs.it)} //retrieve the iterators BST iterator and use it to construct our own
-   { 
-   }
-   iterator & operator = (const iterator & rhs)
-   {
+            erase(it);
+            return 1;
+        }
+        iterator erase(iterator& itBegin, iterator& itEnd)
+        {
+            while (itBegin != itEnd)
+            {
+                itBegin = erase(itBegin);
+            }
+            return itEnd;
+        }
 
-       it = rhs.it; //make the right hand sides iterator equal to ours
-      return *this;
-   }
+    private:
 
-   // equals, not equals operator
-   bool operator != (const iterator & rhs) const 
-   { 
-      return (this->it != rhs.it);
-   }
-   bool operator == (const iterator & rhs) const 
-   { 
+        custom::BST <T> bst;
+    };
 
-      return (this->it==rhs.it);
-   }
 
-   // dereference operator: by-reference so we can modify the Set
-   const T & operator * () const 
-   { 
-      return *this->it; 
-   }
+    /**************************************************
+     * SET ITERATOR
+     * An iterator through Set
+     *************************************************/
+    template <typename T>
+    class set <T> ::iterator
+    {
+        friend class ::TestSet; // give unit tests access to the privates
+        friend class custom::set<T>;
 
-   // prefix increment
-   iterator & operator ++ ()
-   {
-       it++;
-      return *this;
-   }
+    public:
+        // constructors, destructors, and assignment operator
+        iterator() : it{ BST<T>::iterator() }
+        {
+        }
+        iterator(const typename custom::BST<T>::iterator& itRHS) : it{ BST<T>::iterator(itRHS) } //use the paramater BST iterator to create our own
+        {
+        }
+        iterator(const iterator& rhs) : it{ BST<T>::iterator(rhs.it) } //retrieve the iterators BST iterator and use it to construct our own
+        {
+        }
+        iterator& operator = (const iterator& rhs)
+        {
 
-   // postfix increment
-   iterator operator++ (int postfix)
-   {
-       iterator previous = iterator(this);
-       it++;
-      return previous;
-   }
-   
-   // prefix decrement
-   iterator & operator -- ()
-   {
-       it--;
-      return *this;
-   }
-   
-   // postfix decrement
-   iterator operator-- (int postfix)
-   {
-       iterator previous = iterator(this);
-       it--;
-       return previous;
-   }
-   
-private:
+            it = rhs.it; //make the right hand sides iterator equal to ours
+            return *this;
+        }
 
-   typename custom::BST<T>::iterator it;
-};
+        // equals, not equals operator
+        bool operator != (const iterator& rhs) const
+        {
+            return (this->it != rhs.it);
+        }
+        bool operator == (const iterator& rhs) const
+        {
 
-/***********************************************
- * SET : EQUIVALENCE
- * See if two sets are the same size
- ***********************************************/
-template <typename T>
-bool operator == (const set <T> & lhs, const set <T> & rhs)
-{
-   return true;
-}
+            return (this->it == rhs.it);
+        }
 
-template <typename T>
-inline bool operator != (const set <T> & lhs, const set <T> & rhs)
-{
-   return true;
-}
+        // dereference operator: by-reference so we can modify the Set
+        const T& operator * () const
+        {
+            return *this->it;
+        }
 
-/***********************************************
- * SET : RELATIVE COMPARISON
- * See if one set is lexicographically before the second
- ***********************************************/
-template <typename T>
-bool operator < (const set <T> & lhs, const set <T> & rhs)
-{
-   return true;
-}
+        // prefix increment
+        iterator& operator ++ ()
+        {
+            it++;
+            return *this;
+        }
 
-template <typename T>
-inline bool operator > (const set <T> & lhs, const set <T> & rhs)
-{
-   return true;
-}
+        // postfix increment
+        iterator operator++ (int postfix)
+        {
+            iterator previous = iterator(this);
+            it++;
+            return previous;
+        }
+
+        // prefix decrement
+        iterator& operator -- ()
+        {
+            it--;
+            return *this;
+        }
+
+        // postfix decrement
+        iterator operator-- (int postfix)
+        {
+            iterator previous = iterator(this);
+            it--;
+            return previous;
+        }
+
+    private:
+
+        typename custom::BST<T>::iterator it;
+    };
+
+    /***********************************************
+     * SET : EQUIVALENCE
+     * See if two sets are the same size
+     ***********************************************/
+    template <typename T>
+    bool operator == (const set <T>& lhs, const set <T>& rhs)
+    {
+        return true;
+    }
+
+    template <typename T>
+    inline bool operator != (const set <T>& lhs, const set <T>& rhs)
+    {
+        return true;
+    }
+
+    /***********************************************
+     * SET : RELATIVE COMPARISON
+     * See if one set is lexicographically before the second
+     ***********************************************/
+    template <typename T>
+    bool operator < (const set <T>& lhs, const set <T>& rhs)
+    {
+        return true;
+    }
+
+    template <typename T>
+    inline bool operator > (const set <T>& lhs, const set <T>& rhs)
+    {
+        return true;
+    }
 
 }; // namespace custom
-
-
-
